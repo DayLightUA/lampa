97319c9cfec90cfb2aa04cb89a23965f3e08245d
Pavlo Zayarnyuk <zayarnyukpm@gmail.com>
2025-04-12 22:19:28 +0300

    add test plugin

--- test-plugin.js
+++ test-plugin.js
@@ -4,6 +4,6 @@
     // Hook into LAMPA 'select' event
     Lampa.Listener.follow('select', function(e){
         console.log('🔄 Select event triggered!', e);
-        Lampa.Noty.show('✅ Select event triggered!\nevent:' + JSON.stringify(e));
+        Lampa.Noty.show('✅ Select event triggered!');
     });
 })();
