b69ce6f507568e8422e81fcf6a1b5a93e8178a1d
Pavlo Zayarnyuk <zayarnyukpm@gmail.com>
2025-04-12 14:52:49 +0300

    add test plugin

--- test.js
+++ test.js
@@ -1,10 +1,17 @@
 (function(){
+    console.log('💡 Plugin test.js loaded!');
+
     Lampa.Plugins.add('hello-test',{
         title: 'Hello Test Plugin',
         version: '1.0',
-        description: 'Just a test',
+        description: 'Just a test plugin',
         run: function(){
-            Lampa.Noty.show('Hello from plugin!');
+            console.log('🚀 Running hello-test plugin!');
+            try {
+                Lampa.Noty.show('Hello from Test Plugin!');
+            } catch (e) {
+                console.error('❌ Error in plugin:', e);
+            }
         }
     });
 })();
