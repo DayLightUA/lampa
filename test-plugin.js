97b2049ad8eea58f81b65be37ecd7da0d27a1393
Pavlo Zayarnyuk <zayarnyukpm@gmail.com>
2025-04-13 11:20:21 +0300

    add test plugin

--- test-plugin.js
+++ test-plugin.js
@@ -1,14 +1,14 @@
-(function(){
+(function () {
     console.log('💡 Plugin Loaded: Sending formatted logs to Spring Boot API');
 
     try {
-        Lampa.Listener.follow('torrent', function(data){
+        Lampa.Listener.follow('torrent', function (data) {
             if (data && data.type === 'onenter') {
                 try {
                     // Convert to formatted JSON
                     console.log('🧲 Sending Torrent Event Data to API:');
                     const formatted = JSON.stringify(data, null, 2);
-                    sendLogToAPI(formatted);
+                    sendLogToAPI("Event: {0}", [formatted]);
                 } catch (jsonErr) {
                     console.error('❌ JSON stringify failed:', jsonErr);
                 }
@@ -16,12 +16,17 @@
         });
 
         // Function to send log data to your Spring Boot API
-        function sendLogToAPI(logLine) {
+        function sendLogToAPI(message, args) {
             const apiUrl = 'http://192.168.31.104:9292/log'; // Replace with your API URL
+            const base64Args = [];
+            for (const arg of args) {
+                base64Args.push(btoa(arg));
+            }
             const payload = [{
                 timestamp: new Date().toISOString(),
                 appName: "lampa",
-                msg: logLine
+                pattern: message,
+                base64Args: base64Args
             }];
 
             fetch(apiUrl, {
@@ -31,9 +36,9 @@
                 },
                 body: JSON.stringify(payload)
             })
-            .then(response => response.json())
-            .then(data => console.log('✅ Log sent to Spring Boot API:', data))
-            .catch(err => console.error('❌ Failed to send log to API:', err));
+                .then(response => response.json())
+                .then(data => console.log('✅ Log sent to Spring Boot API:', data))
+                .catch(err => console.error('❌ Failed to send log to API:', err));
         }
     } catch (err) {
         console.error('❌ Plugin Error:', err);
